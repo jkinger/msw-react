c6645618ee7587ad89bbe1a448e209e42cb2b6d0
Josh King <josh.king@charter.com>
2024-03-28 14:32:04 -0600

    remove dev check

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
+  // if (process.env.NODE_ENV !== 'development') {
+  //   return
+  // }
  
   const { worker } = await import('./mocks/browser')
  
